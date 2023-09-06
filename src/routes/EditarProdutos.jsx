3b490a59bd977472e3fc5a986b67f2cc6d882c50
crushedbyamallet <rm550242@fiap.com.br>
2023-09-06 12:13:19 -0300

    id request editar prod

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -3,7 +3,9 @@ import { ListaProdutos } from "../components/ListaProdutos";
 
 export default function EditarProdutos() {
   //Recuperando o id que chega no request através do PATH.
-  //Vamos urtilizar o Hook useParams().
+  //Vamos utilizar o Hook useParams().
+
+  document.title = "editar produto";
 
   const {id} = useParams();
 
@@ -20,6 +22,18 @@ export default function EditarProdutos() {
 
   return (
     <>
+      <div>
+        <form>
+          <fieldset>
+            <legend>produto selecionado</legend>
+            <div>
+              <label htmlFor="idNome">Nome</label>
+              <input type="text" name="nome" id="idNome"
+              defaultValue={produtoRecuperado.nome}/>
+            </div>
+          </fieldset>
+        </form>
+      </div>
       <h1>EditarProdutos</h1>
       <h2>PRODUTO</h2>
       <p>NOME : - {objProdutoRecuperado.nome}</p>
